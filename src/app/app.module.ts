682f207dd3a0b1d4c35fd1dc797caa96141a0741
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-04-19 23:18:21 -0300

    [M1S08] Ex01 - tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+
+ formValid = true;
+
+ _form: FormGroup = this.formBuilder.group({
+    usuario: ['', [Validators.required, Validators.minLength(5)]],
+    senha: ['', [Validators.required, Validators.minLength(8)]]
+
+  })
+
+  constructor(
+    private formBuilder: FormBuilder) { }
+
+
+submit() {
+  console.log(this._form.value)
+  if (this._form.valid) {
+    console.log(this._form.valid)
+  }
+  else {
+    alert('Formulário inválido')
+  }
+}
+
+ngOnInit(): void {
+
+
+}
+
+}
