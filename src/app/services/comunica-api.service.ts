f81bf8da0b850d9a85663467478bad5d8da7c828
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-04-22 15:20:36 -0300

    Ex 7 - Service e chamada por componente

--- src/app/services/comunica-api.service.ts
+++ src/app/services/comunica-api.service.ts
@@ -11,17 +11,13 @@ export class ComunicaAPIService {
   constructor(private http: HttpClient) { }
 
   public requisicaoGet(codigoFilme: any, headers: any) {
-    console.log('entrou na API')
     return new Promise((resolve, reject) => {
-      console.log('Fez chamada')
-      // const path = `${environment.filmesAPI}+${codigoFilme}?api_key=environment.apiKey`
       this.http.get(environment.filmesAPI + codigoFilme + '?api_key='+ environment.apiKey, { headers: headers }).toPromise().then((data: any) => {
         resolve(data)
       }).catch(err => {
         console.log('-- catch no get --')
         console.log(err)
         reject(err)
-        // resolve(null)
       })
 
     });
