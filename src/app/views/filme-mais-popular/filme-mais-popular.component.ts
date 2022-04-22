ab445b4ba772782e2f1f951d26860b659ea09680
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-04-22 18:56:03 -0300

    Ex 10 - Adicione botões anterior/próximo

--- src/app/views/filme-mais-popular/filme-mais-popular.component.ts
+++ src/app/views/filme-mais-popular/filme-mais-popular.component.ts
@@ -14,6 +14,8 @@ export class FilmeMaisPopularComponent implements OnInit {
 
   result: any;
   imgFilme = ''
+  arrayFilmes = [];
+  index = 1;
 
   constructor(private _service1: ComunicaAPIService) { }
 
@@ -24,10 +26,35 @@ export class FilmeMaisPopularComponent implements OnInit {
 
  async realizaGet(){
   await this._service1.requisicaoGetPopular(this.headers).then((data: any) => {
-    this.result = data.results[1];
+    this.arrayFilmes = data.results;
+    this.result = data.results[this.index];
     this.imgFilme = 'https://image.tmdb.org/t/p/original/' +  this.result.poster_path;
     
   }).catch((data: any) => {console.log })
  } 
+
+ acaoRetrocede(){
+    if (this.index === 1){
+      alert('Não é possível retroceder');
+    }
+    else{
+      this.index--;
+      this.result = this.arrayFilmes[this.index];
+      this.imgFilme = 'https://image.tmdb.org/t/p/original/' +  this.result.poster_path;
+    }
+ }
+
+ acaoAvanca(){
+  if (this.index === 3){
+    alert('Não é possível avançar');
+  }
+  else{
+    this.index++;
+    this.result = this.arrayFilmes[this.index];
+    this.imgFilme = 'https://image.tmdb.org/t/p/original/' +  this.result.poster_path;
+  }
+
+
+ }
   
 }
